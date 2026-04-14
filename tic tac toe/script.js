d73b17e1115e3b352ac8e4e70e940c66d4f5cd87
Rajeswarimadala <rm6622351@gmail.com>
2026-04-14 18:03:34 +0530

    Add files via upload

---
+++ tic tac toe/script.js
@@ -0,0 +1,181 @@
+/*let boxes=document.querySelectorAll(".box");
+let resetbutton=document.querySelector("#reset-button");
+let newGameButton=document.querySelector("#new-button");
+let msgContainer=document.querySelector(".msg-container");
+let msg=document.querySelector("#msg");
+let turnO=true;//playerX,playerO
+const winPatterns=[
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
+];
+
+
+const resetGame=()=>{
+    turnO=true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+
+
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        
+        if(turnO){
+            box.innerText="O";
+            turnO=false;
+        }else{
+            box.innerText="X";
+            turnO=true;
+        }
+        box.disabled=true;
+        checkWinner();
+    });
+});
+
+const disableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=true;
+    }
+};
+
+
+const enableBoxes=()=>{
+    for(let box of boxes){
+        box.disabled=false;
+        box.innerText="";
+    }
+};
+
+
+const showWinner=()=>{
+    msg.innerText=`Congratulations, winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+const checkWinner=()=>{
+    for(let pattern of winPatterns){
+        let pos1Val=boxes[patterns[0]].innerText;
+        let pos2Val=boxes[patterns[0]].innerText;
+        let pos3Val=boxes[patterns[0]].innerText;
+        if(pos1Val!=""&&pos2Val!=""&&pos3Val!=""){
+            if(pos1Val===pos2Val&&pos2Val===pos3Val){
+        
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+newGameButton.addEventListerner("click",resetGame);
+resetGameButton.addEventListerner("click",resetGame);*/
+
+
+
+
+let boxes = document.querySelectorAll(".box");
+let resetButton = document.querySelector("#reset-button");
+let newGameButton = document.querySelector("#new-button");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // playerO starts
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if(turnO){
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+        checkWinner();
+    });
+});
+
+const disableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = true;
+    }
+};
+
+const enableBoxes = () => {
+    for(let box of boxes){
+        box.disabled = false;
+        box.innerText = "";
+        box.classList.remove("win");
+    }
+};
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations, Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const showDraw = () => {
+    msg.innerText = "It's a Draw!";
+    msgContainer.classList.remove("hide");
+};
+
+const checkWinner = () => {
+    for(let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val !== "" && pos2Val !== "" && pos3Val !== ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                boxes[pattern[0]].classList.add("win");
+                boxes[pattern[1]].classList.add("win");
+                boxes[pattern[2]].classList.add("win");
+                showWinner(pos1Val);
+                return;
+            }
+        }
+    }
+
+    // Check for draw
+    let allFilled = true;
+    boxes.forEach(box => {
+        if(box.innerText === ""){
+            allFilled = false;
+        }
+    });
+    if(allFilled){
+        showDraw();
+    }
+};
+
+newGameButton.addEventListener("click", resetGame);
+resetButton.addEventListener("click", resetGame);
+
+// initialize
+resetGame();
